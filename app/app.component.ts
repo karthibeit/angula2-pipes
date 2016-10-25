794f12ac4a9ae8eb26aee4c8fe25119ed63ea0ef
karthibeit <karthi_beit@rediffmail.com>
2016-10-25 19:31:26 +0530

    First

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+     
+    template: `<h1>Angular Pipes</h1>
+    Actual value : {{stringname}} <br/>
+    After pipe value : {{stringname | removeSpaces}}
+    `
+})
+export class AppComponent { 
+    stringname: string;
+    constructor(){
+        this.stringname = "First pipe";
+    }
+}
